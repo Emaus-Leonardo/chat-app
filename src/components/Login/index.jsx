a00a9fdc2d4e527cc33e63bcaa592097c16d3779
Your Name <emausleonardo123@gmail.com>
2024-07-30 11:44:58 -0300

    STYLE: estilizando page login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { auth, provider } from "../../services/firebase";
 
-const Login = () => {
+export default function Login() {
   const signInWithGoogle = () => {
     auth
       .signInWithPopup(provider)
@@ -17,11 +17,21 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-white">
+      <div className="w-[1100px] h-[609px] flex justify-center items-center bg-[#F8F8F8]">
+        <aside className=""></aside>
+
+        <div className="flex flex-col justify-center items-center p-6 bg-white rounded-lg shadow-md">
+          <h1 className="text-2xl font-bold mb-4">Sign in with Google</h1>
+          <p className="mb-6">Let's chat together.</p>
+          <button
+            onClick={signInWithGoogle}
+            className="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 transition-colors"
+          >
+            Continue with Google!
+          </button>
+        </div>
+      </div>
     </div>
   );
-};
-
-export default Login;
+}
